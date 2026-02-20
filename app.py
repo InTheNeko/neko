79d393420f39baa7f9fc717f7df302f389abfc4e
Neko <kotovtimofey24@gmail.com>
2026-02-20 22:37:49 +0700

    added templates

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template  # Импортируем render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return "<h1>lglglgl!</h1>"
+def home():
+    # Вместо текста возвращаем наш HTML-файл
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
