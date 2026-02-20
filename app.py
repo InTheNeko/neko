0dea14f05fc18c9c88d699631cd927fda4e2ef86
Neko <kotovtimofey24@gmail.com>
2026-02-21 00:15:59 +0700

    added photos and updated design

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-friends = [
+friends_list = [
     ("Руслан", "ruslan"),
     ("Андрей", "andrey"),
     ("Тимоха", "timokha"),
@@ -12,23 +12,26 @@ friends = [
 
 @app.route('/')
 def home():
-    return render_template('index.html', buttons=friends, title="SYSTEM SELECT", is_home=True)
+    return render_template('index.html', buttons=friends_list, title="Выбор профиля", is_home=True)
 
 @app.route('/<page_name>')
 def show_page(page_name):
-    # Данные: ключ -> (Имя, Описание, Файл_фото или None)
-    data = {
+    # Словарь: "ключ": ("Имя", "Описание", "файл_картинки")
+    friends_info = {
         "ruslan": ("Руслан", "Топ игрок, всегда на связи.", "ruslan.jpg"),
-        "andrey": ("Андрей", "Мастер стратегий и тактики.", None), # Без фото
-        "timokha": ("Тимоха", "Главный по юмору в команде.", None), # Без фото
-        "lesha": ("Лёша", "Спокойный и расчетливый игрок.", None), # Без фото
+        "andrey": ("Андрей", "Мастер стратегий.", None), # Без фото
+        "timokha": ("Тимоха", "Главный по юмору.", None),
+        "lesha": ("Лёша", "Никогда не подводит.", None),
         "ibragim": ("Ибрагим", "Легендарный скилл и мощь.", "ibragim.jpg")
     }
     
-    res = data.get(page_name, ("Ошибка", "Профиль не найден", None))
+    data = friends_info.get(page_name, ("Ошибка", "Профиль не найден.", None))
     
     return render_template('index.html', 
-                           title=res[0], 
-                           description=res[1], 
-                           photo=res[2], 
+                           title=data[0], 
+                           description=data[1], 
+                           photo=data[2], # Передаем фото
                            is_home=False)
+
+if __name__ == '__main__':
+    app.run(debug=True)
