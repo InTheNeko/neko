05422edc770bb3b0adef5cfbd5a4846a70773fb3
Neko <kotovtimofey24@gmail.com>
2026-02-20 22:58:13 +0700

    changed buttons to players

--- app.py
+++ app.py
@@ -2,23 +2,40 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Обновленный список кнопок
+menu_items = [
+    ("Игрок 1", "/player1"),
+    ("Игрок 2", "/player2"),
+    ("Игрок 3", "/player3"),
+    ("Игрок 4", "/player4"),
+    ("Игрок 5", "/player5")
+]
+
 @app.route('/')
 def home():
-    # Создаем список: (Название, Ссылка)
-    # Важно: имя переменной 'my_buttons' должно совпадать с тем, что в HTML!
-    my_buttons = [
-        ("Главная", "/"),
-        ("Обо мне", "/about"),
-        ("Проекты", "/projects"),
-        ("Галерея", "/gallery"),
-        ("Контакты", "/contact")
-    ]
-    return render_template('index.html', buttons=my_buttons)
+    return render_template('index.html', 
+                           buttons=menu_items, 
+                           title="Выбор игрока", 
+                           text="Выберите игрока из списка ниже, чтобы просмотреть его профиль.")
 
-# Добавим обработку для всех кнопок, чтобы не было ошибки 404
-@app.route('/<name>')
-def pages(name):
-    return f"<h1>Страница: {name}</h1><a href='/'>Назад на главную</a>"
+@app.route('/<page_name>')
+def show_page(page_name):
+    # Данные для каждого игрока
+    players_info = {
+        "player1": ("Игрок 1", "Характеристики: Сила 10, Ловкость 5."),
+        "player2": ("Игрок 2", "Характеристики: Магия 12, Интеллект 8."),
+        "player3": ("Игрок 3", "Характеристики: Защита 15, Здоровье 100."),
+        "player4": ("Игрок 4", "Характеристики: Скорость 20, Скрытность 10."),
+        "player5": ("Игрок 5", "Характеристики: Удача 7, Меткость 9.")
+    }
+    
+    # Ищем инфо по ключу (например, 'player1')
+    info = players_info.get(page_name, ("Упс!", "Игрок не найден."))
+    
+    return render_template('index.html', 
+                           buttons=menu_items, 
+                           title=info[0], 
+                           text=info[1])
 
 if __name__ == '__main__':
     app.run(debug=True)
