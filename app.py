8e46aa0c6c95d4bb12e9fd6f31fdef0ad1e64b70
Neko <kotovtimofey24@gmail.com>
2026-02-21 00:06:55 +0700

    added futuristic design and photos

--- app.py
+++ app.py
@@ -2,37 +2,34 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Список твоих друзей для главной страницы
-friends_list = [
+# Список для главной: (Имя, ссылка)
+friends = [
     ("Руслан", "ruslan"),
     ("Андрей", "andrey"),
-    ("Тимофка", "timokha"),
+    ("Тимоха", "timokha"),
     ("Лёша", "lesha"),
     ("Ибрагим", "ibragim")
 ]
 
 @app.route('/')
 def home():
-    return render_template('index.html', buttons=friends_list, title="Выбор профиля", is_home=True)
+    return render_template('index.html', buttons=friends, title="Выбор профиля", is_home=True)
 
 @app.route('/<page_name>')
 def show_page(page_name):
-    # Данные для каждого друга (Заголовок, Описание)
-    friends_info = {
-        "ruslan": ("Руслан", "Топ игрок, всегда на связи и готов к каткам. Любит Андрейя"),
-        "andrey": ("Андрей", "Мастер стратегий и просто хороший черт"),
-        "timokha": ("Тимофка", "жаль что он с нами"),
-        "lesha": ("Лёша", "гном всегда гном."),
-        "ibragim": ("Ибрагим", "почти 12 лет")
+    # Словарь: ключ -> (Имя, Описание, Файл_картинки)
+    data = {
+        "ruslan": ("Руслан", "Топ игрок, всегда на связи.", "ruslan.jpg"),
+        "andrey": ("Андрей", "Мастер стратегий.", "andrey.jpg"),
+        "timokha": ("Тимоха", "Главный по юмору.", "timokha.jpg"),
+        "lesha": ("Лёша", "Никогда не подводит.", "lesha.jpg"),
+        "ibragim": ("Ибрагим", "Легендарный скилл.", "ibragim.jpg")
     }
     
-    # Берем данные из словаря
-    data = friends_info.get(page_name, ("Ошибка", "Профиль не найден."))
+    res = data.get(page_name, ("Ошибка", "Не найдено", "default.jpg"))
     
     return render_template('index.html', 
-                           title=data[0], 
-                           description=data[1], 
+                           title=res[0], 
+                           description=res[1], 
+                           photo=res[2], 
                            is_home=False)
-
-if __name__ == '__main__':
-    app.run(debug=True)
