3dcc32e51f2f05da727a4a47629cbaf85d8791a5
Neko <kotovtimofey24@gmail.com>
2026-02-20 22:40:20 +0700

    added logic

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask, render_template  # Импортируем render_template
+from flask import Flask, render_template
+from datetime import datetime # Импортируем время
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    # Вместо текста возвращаем наш HTML-файл
-    return render_template('index.html')
+    current_time = datetime.now().strftime("%H:%M:%S") # Получаем текущее время
+    user_name = "Neko" # Можешь написать свое имя
+    # Передаем переменные в HTML через запятую
+    return render_template('index.html', time=current_time, name=user_name)
 
 if __name__ == '__main__':
     app.run(debug=True)
