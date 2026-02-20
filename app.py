0591dee59b985a94048530334053951dae11dc67
Neko <kotovtimofey24@gmail.com>
2026-02-20 22:46:16 +0700

    added 5 buttons with loop

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
 from flask import Flask, render_template
-from datetime import datetime # Импортируем время
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    current_time = datetime.now().strftime("%H:%M:%S") # Получаем текущее время
-    user_name = "Neko" # Можешь написать свое имя
-    # Передаем переменные в HTML через запятую
-    return render_template('index.html', time=current_time, name=user_name)
+    # Создаем список кнопок: (Текст на кнопке, Ссылка)
+    menu_items = [
+        ("Главная", "/"),
+        ("Обо мне", "/about"),
+        ("Проекты", "/projects"),
+        ("Галерея", "/gallery"),
+        ("Контакты", "/contact")
+    ]
+    return render_template('index.html', menu=menu_items)
+
+# Создадим один универсальный маршрут для всех остальных страниц
+@app.route('/<page_name>')
+def page(page_name):
+    return f"<h1>Вы перешли на страницу: {page_name}</h1><br><a href='/'>Назад на главную</a>"
 
 if __name__ == '__main__':
     app.run(debug=True)
