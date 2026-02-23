aa6b4808a146d24ff1330ced87bc990b1c75b3f0
Neko <kotovtimofey24@gmail.com>
2026-02-23 23:31:15 +0700

    Описание обновления

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Список для кнопок на главной (Имя, URL-путь)
 friends_list = [
     ("Кот", "cat"),
     ("Руслан", "ruslan"),
@@ -13,29 +14,29 @@ friends_list = [
 
 @app.route('/')
 def home():
-    return render_template('index.html', buttons=friends_list, title="Выберите бойца", is_home=True)
+    return render_template('index.html', buttons=friends_list, title="Выберите профиль", is_home=True)
 
 @app.route('/<page_name>')
 def show_page(page_name):
-    # Новая структура: (Имя, Описание, Фото, Ссылки, Статус, Любимая игра)
+    # Данные: "ключ": (0:Имя, 1:Описание, 2:Фото, 3:[Ссылки], 4:Статус, 5:Игра)
     friends_info = {
-       "cat": ("Кот", "Самый главный, у него лапки и власть.", "cat.jpg", 
-                [("Steam", "https://steamcommunity.com/profiles/76561199122830516/"), ("Валерьянка", "#")]),
+        "cat": ("Кот", "Самый главный, у него лапки и власть.", "cat.jpg", 
+                [("Steam", "https://steamcommunity.com"), ("Валерьянка", "#")], "В сети", "Stray"),
         
         "ruslan": ("Руслан", "Ну так ну сяк почти всегда берёт", "ruslan.jpg", 
-                   [("Steam", "https://steamcommunity.com/profiles/76561199198583765/")]),
+                   [("Steam", "https://steamcommunity.com")], "Спит", "Dota 2"),
         
-        "andrey": ("Андрей", "постояно ест", "andrey.jpg", 
-                   [("Steam", "https://steamcommunity.com/profiles/76561198337510525/"), ("я гей", "https://youareanidiot.cc")]),
+        "andrey": ("Андрей", "Постоянно ест, но всё равно тащит.", "andrey.jpg", 
+                   [("Steam", "https://steamcommunity.com"), ("Секрет", "https://youareanidiot.cc")], "В игре", "CS 2"),
         
-        "timokha": ("Тимофка", "жаль что он с нами.", "timokha.jpg", 
-                    [("Steam", "https://steamcommunity.com/profiles/76561199054205841/"), ]),
+        "timokha": ("Тимофка", "Жаль, что он с нами (но мы любя).", "timokha.jpg", 
+                    [("Steam", "https://steamcommunity.com")], "Offline", "Roblox"),
         
-        "lesha": ("Лёша", "гном всегда гном.", "lesha.jpg", 
-                  [("Steam", "https://steamcommunity.com/profiles/76561199096404881/"), ]),
+        "lesha": ("Лёша", "Гном всегда гном. Мал, да удал.", "lesha.jpg", 
+                  [("Steam", "https://steamcommunity.com")], "В сети", "Lethal Company"),
         
-        "ibragim": ("Ибрагим", "почти скоро 12.", "ibragim.jpg", 
-                    [("Steam", "https://steamcommunity.com/profiles/76561199556449044/"), ])
+        "ibragim": ("Ибрагим", "Почти скоро 12. Растёт не по дням.", "ibragim.jpg", 
+                    [("Steam", "https://steamcommunity.com")], "Учит уроки", "Minecraft")
     }
     
     data = friends_info.get(page_name)
