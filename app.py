ad439c8f9b287ad307c8bbc9cdabe220d65e6d30
kitty <kotovtimofey24@gmail.com>
2026-03-08 01:36:39 +0700

    Фикс лайков и добавление удаления

--- app.py
+++ app.py
@@ -5,31 +5,32 @@ import os
 
 app = Flask(__name__)
 
-# База данных
+# Настройка базы данных SQLite
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'drakon.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-# Модель заметок
+# Модель заметки
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
     author = db.Column(db.String(50), default="Аноним")
-    recipient = db.Column(db.String(50))
-    likes = db.Column(db.Integer, default=0)
+    recipient = db.Column(db.String(50))  # К какому профилю относится
+    likes = db.Column(db.Integer, default=0) # Колонка для лайков
     date_posted = db.Column(db.DateTime, default=datetime.utcnow)
 
+# Создаем базу данных
 with app.app_context():
     db.create_all()
 
-# Список друзей
+# Данные профилей
 friends_info = {
     "cat": ("Кот", "Просто самый лучший", "cat.jpg", [("Steam", "#")]),
     "ruslan": ("Руслан", "Гей и любит андрея", "ruslan.jpg", [("Steam", "#")]),
     "andrey": ("Андрей", "Опять ест", "andrey.jpg", [("Steam", "#")]),
     "timokha": ("Тимофка", "Жаль что он с нами", "timokha.jpg", [("Steam", "#")]),
-    "lesha": ("Лёша", "Низки не удаленький", "lesha.jpg", [("Steam", "#")]),
+    "lesha": ("Лёша", "Низкий не удаленький", "lesha.jpg", [("Steam", "#")]),
     "ibragim": ("Ибрагим", "Почти 12 лет ", "ibragim.jpg", [("Steam", "#")])
 }
 
@@ -43,24 +44,4 @@ def show_page(page_name):
     data = friends_info.get(page_name)
     if data:
         name, desc, photo, links = data
-        notes = Note.query.filter_by(recipient=page_name).order_by(Note.date_posted.desc()).all()
-        return render_template('index.html', title=name, description=desc, photo=photo, 
-                               links=links, is_home=False, notes=notes, page_id=page_name)
-    return "Пользователь не найден", 404
-
-@app.route('/add_note/<recipient>', methods=['POST'])
-def add_note(recipient):
-    content = request.form.get('content')
-    author = request.form.get('author') or "Аноним"
-    if content:
-        new_note = Note(content=content, author=author, recipient=recipient, likes=0)
-        db.session.add(new_note)
-        db.session.commit()
-    return redirect(url_for('show_page', page_name=recipient))
-
-@app.route('/like/<int:note_id>', methods=['POST'])
-def like_note(note_id):
-    note = Note.query.get_or_404(note_id)
-    note.likes += 1
-    db.session.commit()
-    return redirect(url_for('show_page', page_name=note.recipient))
+        notes = Note.query.filter_by
