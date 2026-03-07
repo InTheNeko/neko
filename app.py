9d5420f847e8bd472c3732f4fcf7dab2c7548329
kitty <kotovtimofey24@gmail.com>
2026-03-08 01:08:43 +0700

    Обновил дизайн профилей и добавил систему заметок

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from datetime import datetime
 import os
 
 app = Flask(__name__)
+
+# Настройка БД
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'drakon.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -13,26 +15,34 @@ class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
     author = db.Column(db.String(50), default="Аноним")
-    recipient = db.Column(db.String(50)) # Для кого эта заметка
+    recipient = db.Column(db.String(50))
     date_posted = db.Column(db.DateTime, default=datetime.utcnow)
 
 with app.app_context():
     db.create_all()
 
 friends_info = {
-    "cat": ("Кот", "Просто самый лучший", "cat.jpg", [("Steam", "#"), ("Discord", "#")]),
-    "ruslan": ("Руслан", "Гей и любит Андрея", "ruslan.jpg", [("Steam", "#")]),
-    "andrey": ("Андрей", "Опять ест", "andrey.jpg", [("VK", "#")]),
-    "timokha": ("Тимофка", "Жаль, что он с нами", "timokha.jpg", [("Steam", "#")]),
-    "lesha": ("Лёша", "Низкий, но удаленький", "lesha.jpg", [("Telegram", "#")]),
-    "ibragim": ("Ибрагим", "Почти 12 лет", "ibragim.jpg", [("Steam", "#")])
+    "cat": ("Кот", "Просто самый лучший", "cat.jpg", [("Steam", "https://steamcommunity.com")]),
+    "ruslan": ("Руслан", "Гей и любит андрея", "ruslan.jpg", [("Steam", "https://steamcommunity.com")]),
+    "andrey": ("Андрей", "Опять ест", "andrey.jpg", [("Steam", "https://steamcommunity.com")]),
+    "timokha": ("Тимофка", "Жаль что он с нами", "timokha.jpg", [("Steam", "https://steamcommunity.com")]),
+    "lesha": ("Лёша", "Низки не удаленький", "lesha.jpg", [("Steam", "https://steamcommunity.com")]),
+    "ibragim": ("Ибрагим", "Почти 12 лет", "ibragim.jpg", [("Steam", "https://steamcommunity.com")])
 }
 
 @app.route('/')
 def home():
-    # На главной показываем список кнопок
     friends_list = [(v[0], k) for k, v in friends_info.items()]
-    return render_template('index.html', buttons=friends_list, title="DRAKONCHIK OS", is_home=True)
+    return render_template('index.html', buttons=friends_list, title="DRAKONCHIK V3", is_home=True)
+
+@app.route('/<page_name>')
+def show_page(page_name):
+    data = friends_info.get(page_name)
+    if data:
+        name, desc, photo, links = data
+        notes = Note.query.filter_by(recipient=page_name).order_by(Note.date_posted.desc()).all()
+        return render_template('index.html', title=name, description=desc, photo=photo, links=links, is_home=False, notes=notes, page_id=page_name)
+    return "404", 404
 
 @app.route('/add_note/<recipient>', methods=['POST'])
 def add_note(recipient):
@@ -43,18 +53,6 @@ def add_note(recipient):
         db.session.add(new_note)
         db.session.commit()
     return redirect(url_for('show_page', page_name=recipient))
-
-@app.route('/<page_name>')
-def show_page(page_name):
-    data = friends_info.get(page_name)
-    if data:
-        name, desc, photo, links = data
-        # Берем заметки только для ЭТОГО профиля
-        notes = Note.query.filter_by(recipient=page_name).order_by(Note.date_posted.desc()).all()
-        return render_template('index.html', title=name, description=desc, 
-                               photo=photo, links=links, is_home=False, 
-                               notes=notes, page_id=page_name)
-    return "404", 404
 
 if __name__ == '__main__':
     app.run(debug=True)
