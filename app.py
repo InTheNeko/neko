bca28ab2d4cb21b27a05f25e6c44556392886172
kitty <kotovtimofey24@gmail.com>
2026-03-09 21:34:54 +0700

    Вернул стабильную версию без регистрации

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 import os
-from datetime import datetime
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
-app.secret_key = "dragon_social_2026"
+app.secret_key = "dragon_ultra_edition_2026"
 
+# Настройка путей
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'drakon.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['UPLOAD_FOLDER'] = os.path.join(basedir, 'static', 'images')
+app.config['UPLOAD_FOLDER'] = os.path.join(basedir, 'static', 'uploads')
 
 if not os.path.exists(app.config['UPLOAD_FOLDER']):
     os.makedirs(app.config['UPLOAD_FOLDER'])
@@ -25,11 +25,9 @@ class UserProfile(db.Model):
     display_name = db.Column(db.String(100))
     bio = db.Column(db.String(500))
     photo = db.Column(db.String(100), default="default.jpg")
-    # Новые поля для соцсетей
+    video = db.Column(db.String(100), default="")
     tg_link = db.Column(db.String(100), default="")
     vk_link = db.Column(db.String(100), default="")
-
-# Убрали таблицу Note (комментарии)
 
 with app.app_context():
     db.create_all()
@@ -56,10 +54,14 @@ def home():
 @app.route('/user/<page_name>')
 def show_page(page_name):
     user = UserProfile.query.filter_by(username=page_name).first_or_404()
+    
     photo_path = os.path.join(app.config['UPLOAD_FOLDER'], user.photo)
-    photo_url = user.photo if os.path.exists(photo_path) else "default.jpg"
+    photo_url = user.photo if user.photo and os.path.exists(photo_path) else "default.jpg"
+    
+    video_url = user.video if user.video and os.path.exists(os.path.join(app.config['UPLOAD_FOLDER'], user.video)) else None
+    
     return render_template('index.html', user=user, current_photo=photo_url, 
-                           is_home=False, page_id=page_name)
+                           current_video=video_url, is_home=False, page_id=page_name)
 
 @app.route('/edit_profile/<page_name>', methods=['POST'])
 def edit_profile(page_name):
@@ -71,11 +73,17 @@ def edit_profile(page_name):
     user.tg_link = request.form.get('tg_link')
     user.vk_link = request.form.get('vk_link')
     
-    file = request.files.get('photo')
-    if file and file.filename != '':
-        filename = secure_filename(f"avatar_{page_name}.jpg")
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        user.photo = filename
+    photo_file = request.files.get('photo')
+    if photo_file and photo_file.filename != '':
+        p_filename = secure_filename(f"avatar_{page_name}.jpg")
+        photo_file.save(os.path.join(app.config['UPLOAD_FOLDER'], p_filename))
+        user.photo = p_filename
+
+    video_file = request.files.get('video')
+    if video_file and video_file.filename != '':
+        v_filename = secure_filename(f"video_{page_name}.mp4")
+        video_file.save(os.path.join(app.config['UPLOAD_FOLDER'], v_filename))
+        user.video = v_filename
         
     db.session.commit()
     return redirect(url_for('show_page', page_name=page_name))
