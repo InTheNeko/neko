019ee8aa953aa38ac2ba34be5095560aa3bb1f5a
kitty <kotovtimofey24@gmail.com>
2026-03-10 20:25:17 +0700

    Фикс лайков и добавление удаления

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ from flask_sqlalchemy import SQLAlchemy
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
-app.secret_key = "dragon_mega_update_2026"
+app.secret_key = "dragon_final_ultra_fix"
 
+# Настройка путей
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'drakon.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -24,7 +25,6 @@ class UserProfile(db.Model):
     bio = db.Column(db.String(500))
     photo = db.Column(db.String(100), default="default.jpg")
     video = db.Column(db.String(100), default="")
-    # Поле для хранения имен доп. фото через запятую
     gallery = db.Column(db.Text, default="") 
     tg_link = db.Column(db.String(100), default="")
     steam_link = db.Column(db.String(100), default="")
@@ -55,7 +55,7 @@ def home():
 @app.route('/user/<page_name>')
 def show_page(page_name):
     user = UserProfile.query.filter_by(username=page_name).first_or_404()
-    gallery_list = user.gallery.split(',') if user.gallery else []
+    gallery_list = [img for img in user.gallery.split(',') if img] if user.gallery else []
     return render_template('index.html', user=user, gallery=gallery_list, is_home=False, page_id=page_name)
 
 @app.route('/edit_profile/<page_name>', methods=['POST'])
@@ -65,28 +65,24 @@ def edit_profile(page_name):
     
     user = UserProfile.query.filter_by(username=page_name).first_or_404()
     
-    # Обновление текста
     if 'description' in request.form:
         user.bio = request.form.get('description')
         user.tg_link = request.form.get('tg_link')
         user.steam_link = request.form.get('steam_link')
         user.discord_tag = request.form.get('discord_tag')
     
-    # Одиночное фото (Аватар)
     photo_file = request.files.get('photo')
     if photo_file and photo_file.filename != '':
         p_filename = secure_filename(f"avatar_{page_name}_{photo_file.filename}")
         photo_file.save(os.path.join(app.config['UPLOAD_FOLDER'], p_filename))
         user.photo = p_filename
 
-    # Видео
     video_file = request.files.get('video')
     if video_file and video_file.filename != '':
         v_filename = secure_filename(f"video_{page_name}_{video_file.filename}")
         video_file.save(os.path.join(app.config['UPLOAD_FOLDER'], v_filename))
         user.video = v_filename
 
-    # Галерея (Много фото)
     gallery_files = request.files.getlist('gallery_photos')
     new_photos = []
     for f in gallery_files:
@@ -96,7 +92,7 @@ def edit_profile(page_name):
             new_photos.append(g_filename)
     
     if new_photos:
-        current_gal = user.gallery.split(',') if user.gallery else []
+        current_gal = [img for img in user.gallery.split(',') if img] if user.gallery else []
         user.gallery = ",".join(current_gal + new_photos)
         
     db.session.commit()
