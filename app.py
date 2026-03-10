d35e2d538d454702339d45be4cc1be777867ea8e
kitty <kotovtimofey24@gmail.com>
2026-03-10 20:44:14 +0700

    Фикс лайков и добавление удаления

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ from flask_sqlalchemy import SQLAlchemy
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
-app.secret_key = "dragon_multi_video_v1"
+app.secret_key = "drakonchik_ultimate_v3"
 
+# Настройка путей
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'drakon.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -23,18 +24,25 @@ class UserProfile(db.Model):
     display_name = db.Column(db.String(100))
     bio = db.Column(db.String(500))
     photo = db.Column(db.String(100), default="default.jpg")
-    video = db.Column(db.Text, default="") # Теперь тут храним список имен через запятую
-    gallery = db.Column(db.Text, default="") 
+    video = db.Column(db.Text, default="")  # Список видео через запятую
+    gallery = db.Column(db.Text, default="") # Список фото через запятую
     tg_link = db.Column(db.String(100), default="")
     steam_link = db.Column(db.String(100), default="")
     discord_tag = db.Column(db.String(100), default="")
 
 with app.app_context():
     db.create_all()
-    squad = {"cat":"Кот", "ruslan":"Руслан", "andrey":"Андрей", "timokha":"Тимофка", "lesha":"Лёша", "ibragim":"Ибрагим"}
+    squad = {
+        "cat":"Кот", 
+        "ruslan":"Руслан", 
+        "andrey":"Андрей", 
+        "timokha":"Тимофка", 
+        "lesha":"Лёша", 
+        "ibragim":"Ибрагим"
+    }
     for nick, name in squad.items():
         if not UserProfile.query.filter_by(username=nick).first():
-            db.session.add(UserProfile(username=nick, display_name=name, bio="Описание пока пустое..."))
+            db.session.add(UserProfile(username=nick, display_name=name, bio="Боец Drakonchik Squad"))
     db.session.commit()
 
 @app.route('/')
@@ -45,7 +53,6 @@ def home():
 @app.route('/user/<page_name>')
 def show_page(page_name):
     user = UserProfile.query.filter_by(username=page_name).first_or_404()
-    # Разбиваем строки в списки для вывода
     gallery_list = [img for img in user.gallery.split(',') if img] if user.gallery else []
     video_list = [vid for vid in user.video.split(',') if vid] if user.video else []
     return render_template('index.html', user=user, gallery=gallery_list, videos=video_list, is_home=False, page_id=page_name)
@@ -53,7 +60,8 @@ def show_page(page_name):
 @app.route('/edit_profile/<page_name>', methods=['POST'])
 def edit_profile(page_name):
     if request.form.get('admin_password') != ADMIN_PASSWORD:
-        return "Неверный пароль", 403
+        return "Неверный код доступа!", 403
+    
     user = UserProfile.query.filter_by(username=page_name).first_or_404()
     
     if request.form.get('clear_video'): user.video = ""
@@ -67,28 +75,28 @@ def edit_profile(page_name):
     # Аватар
     f_photo = request.files.get('photo')
     if f_photo and f_photo.filename:
-        name = secure_filename(f"at_{page_name}_{f_photo.filename}")
+        name = secure_filename(f"ava_{page_name}_{f_photo.filename}")
         f_photo.save(os.path.join(app.config['UPLOAD_FOLDER'], name))
         user.photo = name
 
-    # Загрузка нескольких Видео
+    # Видео (множественно)
     f_videos = request.files.getlist('video')
     new_vids = []
     for f in f_videos:
         if f.filename:
-            name = secure_filename(f"vi_{page_name}_{f.filename}")
+            name = secure_filename(f"vid_{page_name}_{f.filename}")
             f.save(os.path.join(app.config['UPLOAD_FOLDER'], name))
             new_vids.append(name)
     if new_vids:
         old_vids = [v for v in user.video.split(',') if v] if user.video else []
         user.video = ",".join(old_vids + new_vids)
 
-    # Фото галерея
+    # Галерея (множественно)
     f_gallery = request.files.getlist('gallery')
     new_imgs = []
     for f in f_gallery:
         if f.filename:
-            name = secure_filename(f"ga_{page_name}_{f.filename}")
+            name = secure_filename(f"gal_{page_name}_{f.filename}")
             f.save(os.path.join(app.config['UPLOAD_FOLDER'], name))
             new_imgs.append(name)
     if new_imgs:
